6e6d0cd10fc92b0988f4e18fb7e93269c301d4cd
Hugo Torres <hugo.torres1993@gmail.com>
2015-04-19 17:21:37 -0400

    Fetch data from MongoDB.
    A database connection is pretty simple to set up.

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 var express = require('express');
 var app = express();
+var db = require('monk')('localhost:27017/simple-appdb');
 
 app.use(express.static('public'));
 
+// A little DB middle-ware
+app.use(function(req, res, next) {
+  req.db = db;
+  next();
+});
+
 app.get('/posts', function(req, res) {
   console.log("GET /posts");
-
-  var data = require('./data.json');
-
-  res.statusCode = 200;
-  res.send(JSON.stringify(data));
+  
+  db.get('posts').find({})
+    .success(function(docs) {
+      res.statusCode = 200;
+      res.send(JSON.stringify(docs));
+    })
+    .error(function(docs) {
+      res.statusCode = 500;
+      res.send("Error connecting to the database.");
+    });
 });
 
 var server = app.listen(3000, function() {
